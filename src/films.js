8eeb99c61efe4e0167ef588e892ed296ae4946f6
Alejandra Cabello <alejandracabellomunoz@gmail.com>
2022-10-24 14:45:50 +0200

    ejercicio 7

--- src/films.js
+++ src/films.js
@@ -19,18 +19,18 @@ function getMoviesFromProperty(array, propiedad, value) {//OK
   let valor = value;
   console.log(x);
   console.log(valor);
-      switch (x) {
-      case "director": let peliculasDirector = array.filter((movie) => movie.director === value);
-          console.table(peliculasDirector);
-          return peliculasDirector;
-          break;
-      case "genre": let peliculasGenero = array.filter(movie => movie.genre.includes(value));
-          console.table(peliculasGenero);
-          return peliculasGenero;
-          break;
-      case "year": let peliculasAño = array.filter((movie) => movie.year === value);
-          console.table(peliculasAño);
-          return peliculasAño;
+  switch (x) {
+    case "director": let peliculasDirector = array.filter((movie) => movie.director === value);
+      console.table(peliculasDirector);
+      return peliculasDirector;
+      break;
+    case "genre": let peliculasGenero = array.filter(movie => movie.genre.includes(value));
+      console.table(peliculasGenero);
+      return peliculasGenero;
+      break;
+    case "year": let peliculasAño = array.filter((movie) => movie.year === value);
+      console.table(peliculasAño);
+      return peliculasAño;
   }
 }
 
@@ -38,13 +38,13 @@ function getMoviesFromProperty(array, propiedad, value) {//OK
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
   let peliculasDirector = getMoviesFromDirector(array, director);
-    let puntuacionesFiltradas = peliculasDirector.filter(movie => movie.score != undefined);//para array solo con las puntuaciones .map, si quisiera un array 
-    // con objetos .filter
-    let long = puntuacionesFiltradas.length;
-    let total = puntuacionesFiltradas.reduce((acc, movie) => { return acc += parseFloat(movie.score) }, 0);
-    let media = parseFloat((total / long).toFixed(2));
+  let puntuacionesFiltradas = peliculasDirector.filter(movie => movie.score != undefined);//para array solo con las puntuaciones .map, si quisiera un array 
+  // con objetos .filter
+  let long = puntuacionesFiltradas.length;
+  let total = puntuacionesFiltradas.reduce((acc, movie) => { return acc += parseFloat(movie.score) }, 0);
+  let media = parseFloat((total / long).toFixed(2));
 
-    return media;
+  return media;
 
 }
 
@@ -63,16 +63,16 @@ function moviesAverage(array) {//aqui le llega ya el array de peliculas de objet
 // Exercise 4:  Alphabetic order by title 
 function orderAlphabetically(array) {
   console.log("orderAlphabetically");
-    let titulosPeliculas = array.map(movie => movie.title.toLowerCase());//devuelve un array con solo los títulos
-    console.table(titulosPeliculas);
-    //las pongo en minusculas para que ordene alfabeticamente correctamente, que no ponga ET antes que El niño 
-    //las ordeno alfabéticamente:
-    titulosPeliculas.sort();
-    //solo mostrar las 20 primeras peliculas 
-    let primerasPeliculas = titulosPeliculas.filter((element, index) => index < 20);
+  let titulosPeliculas = array.map(movie => movie.title.toLowerCase());//devuelve un array con solo los títulos
+  console.table(titulosPeliculas);
+  //las pongo en minusculas para que ordene alfabeticamente correctamente, que no ponga ET antes que El niño 
+  //las ordeno alfabéticamente:
+  titulosPeliculas.sort();
+  //solo mostrar las 20 primeras peliculas 
+  let primerasPeliculas = titulosPeliculas.filter((element, index) => index < 20);
 
-    console.table(primerasPeliculas);
-    return primerasPeliculas;
+  console.table(primerasPeliculas);
+  return primerasPeliculas;
 
 }
 
@@ -82,13 +82,13 @@ function orderByYear() {
   let peliculasOrdenadas = array.sort((a, b) => {
     if (a.year > b.year) { return 1 }
     if (a.year === b.year) {
-        let ac = (a.title > b.title) ? 1 : -1;
-        return ac;
+      let ac = (a.title > b.title) ? 1 : -1;
+      return ac;
     }//estoy suponiendo que no hay 2 peliculas que se llamen igual
     if (a.year < b.year) { return -1 }
-});
-console.table(peliculasOrdenadas);
-return peliculasOrdenadas;
+  });
+  console.table(peliculasOrdenadas);
+  return peliculasOrdenadas;
 
 }
 
@@ -97,14 +97,35 @@ function moviesAverageByCategory() {
   console.log("moviesAverageByCategory");
   let peliculasGenero = getMoviesFromProperty(array, "genre", category);//getMoviesFromProperty(movies, "genre", value);
   let media = moviesAverage(peliculasGenero);
-     return media;
+  return media;
 
 }
 
 // Exercise 7: Modify the duration of movies to minutes
 function hoursToMinutes() {
-
+  console.log("hoursToMinutes");
+  let peliculasEnMinutos = array.filter(item => item.duration = generarArrayTiempo(item.duration));
+  //console.table(peliculasEnMinutos);
+  return peliculasEnMinutos
 }
+function generarArrayTiempo(string) {//OK, PERO HAY QUE SIMPLIFICAR
+  console.log("generarArrayTiempo");
+
+  let tiempoFinal = 0;
+  if (string.includes("h") && string.includes("min")) {
+    let sinHoras = string.replace("h", "");//quito el caracter h, si lo hago con un parseInt pierdo la parte de min 
+    let sinMinutos = sinHoras.replace("min", "");//quito el caracter min
+    let tiempo = sinMinutos.split(" "); // ahora en duration hay un arry de long 2
+    tiempoFinal = parseInt(tiempo[0] * 60) + parseInt(tiempo[1]);
+  } else if (string.includes("h") && !string.includes("min")) {//solo hay  horas
+    tiempoFinal = parseInt(string);
+    tiempoFinal *= 60;
+  } else if (!string.includes("h") && !string.includes("min")) {//solo hay  minutos
+    tiempoFinal = parseInt(string);
+  }
+  return tiempoFinal;
+}
+
 
 // Exercise 8: Get the best film of a year
 function bestFilmOfYear() {
